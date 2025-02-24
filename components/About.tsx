a3f063dc361aed6e3a44175648686d6d6d184a77
James Huang <jhuang4647@gmail.com>
2025-02-24 08:06:17 -0800

    fixed linting issues in About.tsx and reworded my blurb

--- components/About.tsx
+++ components/About.tsx
@@ -2,13 +2,13 @@ import Link from "next/link";
 
 const About = () => {
   return (
-    <div className='text-gray-900 dark:text-gray-50 font-sans'>
-      <p className='text-xs md:text-sm lg:text-md xl:text-lg mb-4'>
-        I'm an engineer who builds products and experiences that make people's lives easier.
+    <div className='text-gray-900 dark:text-gray-50 font-sans flex flex-col gap-4'>
+      <p className='text-xs md:text-sm lg:text-md xl:text-lg'>
+        First and foremost, I&apos;m an engineer who builds products and experiences that make people&apos;s lives easier and loves to understand every little detail.
       </p>
 
-      <p className="text-xs md:text-sm lg:text-md xl:text-lg mb-4">
-        Currently, I'm a software engineer at{' '}
+      <p className="text-xs md:text-sm lg:text-md xl:text-lg">
+        Currently, I&apos;m a software engineer at{' '}
         <Link 
           href="https://www.chickasaw.com/" 
           className="underline decoration-dashed text-gray-900 dark:text-gray-50 hover:text-gray-700 dark:hover:text-gray-300"
@@ -17,21 +17,22 @@ const About = () => {
         </Link>, a company that works in the government sector.
       </p>
 
-      <p className="text-xs md:text-sm lg:text-md xl:text-lg mb-4">
+      <p className="text-xs md:text-sm lg:text-md xl:text-lg">
         In my spare time, I like to cook, rock climb and run in preparation for my next marathon. 
-        Depending on the season, you'll also catch me snowboarding, playing various park sports (handball, pickleball, etc.), 
+        Depending on the season, you&apos;ll also catch me snowboarding, playing various park sports (handball, pickleball, etc.), 
         paddling in a{' '}
         <Link 
           href="https://www.dchracing.com/" 
           className="underline decoration-dashed text-gray-900 dark:text-gray-50 hover:text-gray-700 dark:hover:text-gray-300"
         >
           dragon boat
-        </Link>, or helping out my lion dance team.
+        </Link>, or helping out my lion dance team. I'm starting to learn tennis this year (&apos;25) too.
       </p>
 
-      <p className="text-xs md:text-sm lg:text-md xl:text-lg mb-4">
-        Check out some of the highlights in my life!
+      <p className='text-xs md:text-sm lg:text-md xl:text-lg'>
+        Thanks for stopping by and learning a bit about my professional and personal life!
       </p>
+
     </div>
   );
 };
