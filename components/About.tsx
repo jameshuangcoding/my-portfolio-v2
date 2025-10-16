9a025bff74c7f8e7aa1db0ddec11e4720a716776
James Huang <jhuang4647@gmail.com>
2025-10-15 21:54:36 -0400

    added new experience to job

--- components/About.tsx
+++ components/About.tsx
@@ -1,38 +1,43 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 const About = () => {
+  const paragraphStyle = 'text-xs md:text-sm lg:text-md xl:text-lg';
+
   return (
     <div className='text-gray-900 dark:text-gray-50 font-sans flex flex-col gap-4'>
-      <p className='text-xs md:text-sm lg:text-md xl:text-lg'>
-        First and foremost, I&apos;m an engineer who builds products and experiences that make people&apos;s lives easier and loves to understand every little detail.
+      <p className={paragraphStyle}>
+        I&apos;m a software engineer passionate about building tools that
+        simplify the complexities of technology. My favorite work involves
+        helping people whether it be developers, teams or anyone looking to use
+        technology to their advantage.
       </p>
 
-      <p className="text-xs md:text-sm lg:text-md xl:text-lg">
-        Currently, I&apos;m a software engineer at{' '}
-        <Link 
-          href="https://www.chickasaw.com/" 
-          className="underline decoration-dashed text-gray-900 dark:text-gray-50 hover:text-gray-700 dark:hover:text-gray-300"
+      <p className={paragraphStyle}>
+        Currently, I&apos;m a software engineer intern at{' '}
+        <Link
+          href='https://www.chickasaw.com/'
+          className='underline decoration-dashed text-gray-900 dark:text-gray-50 hover:text-gray-700 dark:hover:text-gray-300'
         >
-          Chickasaw Nation Industries
-        </Link>, a company that works in the government sector.
+          VGS
+        </Link>
+        , specializing in developer velocity. As part of the Release Team, I
+        focus on release reliability and CI/CD optimization, helping engineering
+        teams build and ship without spending time fighting legacy pipelines.
       </p>
 
-      <p className="text-xs md:text-sm lg:text-md xl:text-lg">
-        In my spare time, I like to cook, rock climb and run in preparation for my next marathon. 
-        Depending on the season, you&apos;ll also catch me snowboarding, playing various park sports (handball, pickleball, etc.), 
-        paddling in a{' '}
-        <Link 
-          href="https://www.dchracing.com/" 
-          className="underline decoration-dashed text-gray-900 dark:text-gray-50 hover:text-gray-700 dark:hover:text-gray-300"
-        >
-          dragon boat
-        </Link>, or helping out my lion dance team. I&apos;m also starting to learn tennis this year (&apos;25) too.
+      <p className={paragraphStyle}>
+        I&apos;m also pursuing a Master&apos;s in Computer Science at Georgia
+        Tech, focusing on machine learning and artifical intelligence. I&apos;m
+        particularly interested in applying AI, backend engineering and DevOps
+        to build intelligent systems and the infrastructure to support model
+        deployment for real word impact.
       </p>
 
-      <p className='text-xs md:text-sm lg:text-md xl:text-lg'>
-        Thanks for stopping by and learning a bit about my professional and personal life!
+      <p className={paragraphStyle}>
+        In my spare time, I&apos;m using climbing, training for my next
+        marathon, or playing Teamfight Tactics (I&apos;m a big fan of games by
+        Riot Games).
       </p>
-
     </div>
   );
 };
--- data/experiences.ts
+++ data/experiences.ts
@@ -1,32 +1,38 @@
 export interface Experience {
-    icon: string;
-    company: string;
-    team: string;
-    time: string;
-    link: string;
-  }
-  
-  export const experiences: Experience[] = [
-    {
-      icon: '/cni-icon.png',
-      company: 'Chickasaw Nation Industries',
-      team: 'Engineering',
-      time: "Current",
-      link: 'https://www.chickasaw.com/',
-    },
-    {
-      icon: '/dev-overflow-icon.png',
-      company: 'Dev-Overflow',
-      team: 'Engineering',
-      time: "2023-24",
-      link: 'https://www.linkedin.com/company/dev-overflow/',
-    },
-    {
-      icon: '/oslabs-icon.png',
-      company: 'OSLabs',
-      team: 'Engineering',
-      time: "2023",
-      link: 'https://www.opensourcelabs.io/',
-    },
-  ];
-  
+  icon: string;
+  company: string;
+  team: string;
+  time: string;
+  link: string;
+}
+
+export const experiences: Experience[] = [
+  {
+    icon: '/vgs-icon.png',
+    company: 'VGS',
+    team: 'Engineering',
+    time: 'Current',
+    link: 'https://www.verygoodsecurity.com/',
+  },
+  {
+    icon: '/cni-icon.png',
+    company: 'Chickasaw Nation Industries',
+    team: 'Engineering',
+    time: '2024-2025',
+    link: 'https://www.chickasaw.com/',
+  },
+  {
+    icon: '/dev-overflow-icon.png',
+    company: 'Dev-Overflow',
+    team: 'Engineering',
+    time: '2023-24',
+    link: 'https://www.linkedin.com/company/dev-overflow/',
+  },
+  {
+    icon: '/oslabs-icon.png',
+    company: 'OSLabs',
+    team: 'Engineering',
+    time: '2023',
+    link: 'https://www.opensourcelabs.io/',
+  },
+];
